ac87389827a29b5506957ce404017ffd527718be
Avery Smith <ajsmith2@wpi.edu>
2019-10-18 00:26:01 -0400

    Add files via upload
    Updates to animation loop to allow exiting at any time instead of waiting for current input to finish, along with formatting of comments.

--- PolarIntro.py
+++ PolarIntro.py
@@ -1,11 +1,18 @@
+# A script that draws circles tangentially around a circle centered on the user's click.
+# Allows options to adjust the radius of the circles, recursions, and number of circles
+# drawn.
+
+# Avery Smith
+
+#########################################################################################
+#                                     Set Up                                            #
+#########################################################################################
+
 from graphics import *
 import math
 from random import seed, randint
 
-# Set up
 
-# Define window width and height
-# IMPORTANT, X AND Y COORDINATES START FROM THE TOP LEFT OF THE SCREEN
 MAX_WIDTH = 1200
 MAX_HEIGHT = 800
 
@@ -13,52 +20,53 @@ seed()
 win = GraphWin('Circles', width = MAX_WIDTH, height = MAX_HEIGHT)
 isRunning = True
 
-# Create an exit rectangle
+# Exit Box
 exitBox = Rectangle(Point(MAX_WIDTH*20/21, 0), Point(MAX_WIDTH, MAX_HEIGHT/21))
 exitBox.setFill('red')
 exitBox.draw(win)
 
-# Exit text
+# Exit text to fill the box
 exitText = Text(Point((MAX_WIDTH+MAX_WIDTH*20/21)/2, MAX_HEIGHT/41), 'Exit')
 exitText.draw(win)
 
-# Create text for the radius box
+# Text for radius input
 radText = Text(Point(MAX_WIDTH/16 + 1, MAX_HEIGHT/32), 'Radius')
 radText.draw(win)
 
-# Create a box to allow for radius change
+# Entry for radius input
 radBox = Entry(Point(MAX_WIDTH/16, MAX_HEIGHT/16), 5)
 radBox.setText('100')
 radBox.draw(win)
 
-# Create text for the recursions box
+# Recursions text
 recText = Text(Point(MAX_WIDTH*3/16 + 170, MAX_HEIGHT/32), 'Number of Recursions (more than 6 can be problematic)')
 recText.draw(win)
 
-# Create a box to allow number of recursions to change
+# Entry for recursions input
 recBox = Entry(Point(MAX_WIDTH*3/16, MAX_HEIGHT/16), 5)
 recBox.setText('3')
 recBox.draw(win)
 
-# Choose how many extra circles per circle
+# Number of circles input
 circBox = Entry(Point(MAX_WIDTH*9/16, MAX_HEIGHT/16), 5)
 circBox.setText('8')
 circBox.draw(win)
 
-# Number of Circles text
+# Number of circles text
 circText = Text(Point(MAX_WIDTH*9/16 + 75, MAX_HEIGHT/32), 'Number of Circles generated')
 circText.draw(win)
 
 
-# Functions
+#########################################################################################
+#                                     Functions                                         #
+#########################################################################################
 
 # An alternative to the range function that allows for floating point types.
-# Range only accepts integers but we want to work with pi.
+# Credit to https://stackoverflow.com/questions/7267226/range-for-floats
 def frange(x, y, jump):
   while x < y:
     yield x
     x += jump
-
 
 # Converts Polar coordinates to cartesian
 def toCartesian(radius, theta):
@@ -86,20 +94,22 @@ def pointWithin(p1, p2, p3):
     return False
 
 # Recursively creates smaller circles with centers along the current
-# circle's edge. Each row of circles will have a random color, but you can
-# change it so each circle is a random color by moving the color variable
-# into the for loop.
+# circle's edge. Each row of circles will have a random color.
 def recursiveCircles(num, numCirc, radi, cent, window):
-  if num > 0:
-    color = randColor()
-    # The number of circles generated is 2 times the number that pi
-    # is being divided by.
-    for i in frange (0, 2*math.pi, math.pi/(numCirc/2)):
-      center2 = addPoints(toCartesian(radi, i), cent)
-      cir2 = Circle(center2, radi/2)
-      cir2.setOutline(color)
-      cir2.draw(window)
-      recursiveCircles(num - 1, numCirc, radi/2, center2, window)
+  click = win.checkMouse()
+  global isRunning
+  if isRunning and (click == None or not pointWithin(click, exitBox.getP1(), exitBox.getP2())):
+    if num > 0:
+      color = randColor()
+      for i in frange (0, 2*math.pi, math.pi/(numCirc/2)):
+        center2 = addPoints(toCartesian(radi, i), cent)
+        cir2 = Circle(center2, radi/2)
+        cir2.setOutline(color)
+        cir2.draw(window)
+        recursiveCircles(num - 1, numCirc, radi/2, center2, window)
+  else:
+    isRunning = False
+      
 
 # Choose a random color from red, green, and blue.
 def randColor():
@@ -112,7 +122,9 @@ def randColor():
     return 'blue'
   
 
-# Main/Run
+#########################################################################################
+#                                     Run                                               #
+#########################################################################################
 
 while isRunning:
   center = win.getMouse()
@@ -127,3 +139,5 @@ while isRunning:
 
     # Create the rest
     recursiveCircles(int(recBox.getText()) - 1, int(circBox.getText()), int(radBox.getText()), center, win)
+
+win.close()
